026c443602c442e0700eaaa1e2b9ec71eb20d864
Hackerboy321 <deepanshukhankriyal4@gmail.com>
2026-03-04 00:28:45 +0530

    reset -mixed commit

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 console.log('Happy Holi') ; 
 console.log("login feature implemented") ; 
 console.log("login feature dug fixed") ;
-console.log("conflict is resolved") ; 
+console.log("conflict is resolved") ;
+
+console.log("now i am learning revert and rest in git") ;
+console.log("now i am commit another change to get better understanding of git") ; 
+console.log("this is third commit for understanding the git better") ; 
